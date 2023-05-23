b6cc09a1d8a0525d816c3a3c18bd0148e406a85a
pengqiu <qiupeng@bu.edu>
2023-05-23 01:15:44 -0400

    adapte to windows

--- webtest.py
+++ webtest.py
@@ -4,7 +4,6 @@ import time
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-import xlrd
 
 caller_xpath = "/html/body/div[1]/div/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div[2]/div/div/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div/a"
 call_btn_xpath = "/html/body/div[1]/div/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/div/div[1]/span/div"
@@ -19,7 +18,7 @@ def main():
 
     # see https://stackoverflow.com/questions/43734797/page-load-strategy-for-chrome-driver-updated-till-selenium-v3-12-0
     options = webdriver.ChromeOptions()
-    options.page_load_strategy = 'eager'
+    # options.page_load_strategy = 'eager'
 
     # see https://stackoverflow.com/questions/38832776/how-do-i-allow-chrome-to-use-my-microphone-programmatically
     # and https://stackoverflow.com/questions/39381088/selenium-python-chrome-open-with-def-options
@@ -28,21 +27,22 @@ def main():
 
     browser = webdriver.Chrome(options=options)
 
-    # # see https://stackoverflow.com/questions/61308799/unable-to-locate-elements-in-selenium-python
-    # browser.get("https://www.messenger.com/login/")
-    # browser.find_element(By.ID, "email").send_keys(myusername)
-    # browser.find_element(By.ID, "pass").send_keys(mypassword)
-    # browser.find_element(By.ID, "loginbutton").click()
+    # see https://stackoverflow.com/questions/61308799/unable-to-locate-elements-in-selenium-python
+    browser.get("https://www.messenger.com/login/")
+    browser.implicitly_wait(1)
+    browser.find_element(By.ID, "email").send_keys(myusername)
+    browser.find_element(By.ID, "pass").send_keys(mypassword)
+    browser.find_element(By.ID, "loginbutton").click()
 
-    # # see https://www.testclass.cn/selenium_iframe.html
-    # print("The caller is loading...")
-    # while(not hasElement(browser, caller_xpath)):
-    #     pass
-    # print("The caller is loaded.")
-    # browser.find_element(By.XPATH, caller_xpath).click()
-    # print("Start calling...")
-    # browser.find_element(By.XPATH, call_btn_xpath).click()
-    # print("Waiting for the call to be answered...")
+    # see https://www.testclass.cn/selenium_iframe.html
+    print("The caller is loading...")
+    while(not hasElement(browser, caller_xpath)):
+        pass
+    print("The caller is loaded.")
+    browser.find_element(By.XPATH, caller_xpath).click()
+    print("Start calling...")
+    browser.find_element(By.XPATH, call_btn_xpath).click()
+    print("Waiting for the call to be answered...")
 
     # if q is pressed, quit
     while(True):
