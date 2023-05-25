84edf5d3406c29747ccc7c63974eb8af4aa17e4b
pengqiu <qiupeng@bu.edu>
2023-05-25 16:46:13 -0400

    call receive function with auto control

--- call_out_test.py
+++ call_out_test.py
@@ -9,6 +9,12 @@ caller_xpath = "/html/body/div[1]/div/div/div/div[2]/div/div/div/div[1]/div[1]/d
 call_btn_xpath = "/html/body/div[1]/div/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/div/div[1]/span/div"
 download_btn_xpath1 = "/html/body/p/details/summary"
 download_btn_xpath2 = "/html/body/p/details/div/div[1]/a/button"
+receiver_name_xpath = "/html/body/div/div/div/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/div/div[1]/span"
+receiver_icon_xpath = "/html/body/div/div/div/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/div/div/div[2]/div/div/div/div/div/div/div[1]/div[2]/div[1]/div/img"
+waiting_sign_xpath = "/html/body/div/div/div/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/div/div[2]/span/span"
+end_call_btn_xpath = "/html/body/div/div/div/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/div/div/div[3]/div/div[1]/div[2]/div/div/div[2]/div[5]/span/div/div/div"
+close_call_btn_xpath = "/html/body/div/div/div/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div[3]/div/div[1]/div[2]/div/div/div[2]/div[2]/button"
+recall_btn_xpath = "/html/body/div/div/div/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div/div/div[2]/div/div/button"
 
 
 def main():
@@ -30,7 +36,7 @@ def main():
 
     # see https://stackoverflow.com/questions/43734797/page-load-strategy-for-chrome-driver-updated-till-selenium-v3-12-0
     options = webdriver.ChromeOptions()
-    # options.page_load_strategy = 'eager'
+    options.page_load_strategy = 'eager'  # for faster loading
 
     # see https://stackoverflow.com/questions/38832776/how-do-i-allow-chrome-to-use-my-microphone-programmatically
     # and https://stackoverflow.com/questions/39381088/selenium-python-chrome-open-with-def-options
@@ -53,30 +59,70 @@ def main():
     browser.find_element(By.ID, "email").send_keys(myusername)
     browser.find_element(By.ID, "pass").send_keys(mypassword)
     browser.find_element(By.ID, "loginbutton").click()
+    contacts_window = browser.current_window_handle
 
     # see https://www.testclass.cn/selenium_iframe.html
-    print("The caller is loading...")
+    print("The caller page is loading...")
     while(not hasElement(browser, caller_xpath)):
         pass
-    print("The caller is loaded.")
+    print("The caller page is loaded.")
     browser.find_element(By.XPATH, caller_xpath).click()
-    print("Start calling...")
-    browser.find_element(By.XPATH, call_btn_xpath).click()
-    print("Waiting for the call to be answered...")
 
     # create a new tab for webrtc-internals and switch to it
     browser.switch_to.new_window('tab')
     browser.get("chrome://webrtc-internals")
     browser.implicitly_wait(1)  # wait for elements to load
-    print("webrtc-internals is loaded.")
+    print("webrtc-internals page is loaded.")
+    rtc_window = browser.current_window_handle
 
-    # if q is pressed, quit
+    browser.switch_to.window(contacts_window)
+    print("Start calling...")
+    browser.find_element(By.XPATH, call_btn_xpath).click()
+    windows = browser.window_handles
+    call_window = windows[2]
+
+    browser.switch_to.window(call_window)
+    while(not hasElement(browser, receiver_name_xpath)):  # wait for the call box to be fully loaded
+        pass
+    print("Waiting for the call to be answered...")
+    browser.implicitly_wait(1)  # wait for elements to load
+    start = time.time()
+    while(hasElement(browser, waiting_sign_xpath)):
+        end = time.time()
+        if((end - start) > 10):
+            print("The call is not answered for 10 seconds.")
+            browser.quit()
+            return
+    if (hasElement(browser, close_call_btn_xpath)):
+        print("The call is not answered.")
+        browser.find_element(By.XPATH, close_call_btn_xpath).click()
+        browser.quit()
+        return
+
+    browser.switch_to.window(call_window)
+    while(not hasElement(browser, receiver_icon_xpath)):  # wait for the call box to be fully loaded
+        pass
+    print("The call is answered.")
+
+    browser.switch_to.window(call_window)
+    start = time.time()
     while(True):
-        if(input() == 'q'):
+        end = time.time()
+        print("The call has been connected for " +
+              str(end - start) + " seconds.", end="\r")
+        # if q is pressed or 10 seconds have passed, quit the program
+        hasRecall = hasElement(browser, recall_btn_xpath)
+        if(((end - start) > 10) or hasRecall):
+            if(not hasRecall):
+                browser.find_element(By.XPATH, end_call_btn_xpath).click()
+            print("\nThe call is ended.")
+            browser.switch_to.window(rtc_window)
             browser.find_element(By.XPATH, download_btn_xpath1).click()
             browser.find_element(By.XPATH, download_btn_xpath2).click()
             sleep(3)  # wait for download to finish
+            print("The dump file is downloaded.")
             break
+
     # clear()
     browser.quit()
 
--- call_receive_test.py
+++ call_receive_test.py
@@ -8,6 +8,9 @@ from selenium.webdriver.support.ui import Select
 receive_btn_xpath = "/html/body/div[4]/div[1]/div/div[2]/div/div/div/div[2]/div/div/div/div[3]/div/div[2]/div/div/div[1]/div/div"
 download_btn_xpath1 = "/html/body/p/details/summary"
 download_btn_xpath2 = "/html/body/p/details/div/div[1]/a/button"
+caller_icon_xpath = "/html/body/div/div/div/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/div/div/div[2]/div/div/div/div/div/div/div[1]/div[2]/div[1]/div/img"
+end_call_btn_xpath = "/html/body/div/div/div/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/div/div/div[3]/div/div[1]/div[2]/div/div/div[2]/div[5]/span/div/div/div"
+recall_btn_xpath = "/html/body/div/div/div/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div/div/div[2]/div/div/button"
 
 
 def main():
@@ -29,7 +32,7 @@ def main():
 
     # see https://stackoverflow.com/questions/43734797/page-load-strategy-for-chrome-driver-updated-till-selenium-v3-12-0
     options = webdriver.ChromeOptions()
-    # options.page_load_strategy = 'eager'
+    options.page_load_strategy = 'eager' # for faster loading
 
     # see https://stackoverflow.com/questions/38832776/how-do-i-allow-chrome-to-use-my-microphone-programmatically
     # and https://stackoverflow.com/questions/39381088/selenium-python-chrome-open-with-def-options
@@ -52,28 +55,54 @@ def main():
     browser.find_element(By.ID, "email").send_keys(myusername)
     browser.find_element(By.ID, "pass").send_keys(mypassword)
     browser.find_element(By.ID, "loginbutton").click()
-
-    # see https://www.testclass.cn/selenium_iframe.html
-    print("Waiting for the call...")
-    while(not hasElement(browser, receive_btn_xpath)):
-        pass
-    browser.find_element(By.XPATH, receive_btn_xpath).click()
-    print("The call is received.")
+    contacts_window = browser.current_window_handle
 
     # create a new tab for webrtc-internals and switch to it
     browser.switch_to.new_window('tab')
     browser.get("chrome://webrtc-internals")
     browser.implicitly_wait(1)  # wait for elements to load
-    print("webrtc-internals is loaded.")
+    print("webrtc-internals page is loaded.")
+    rtc_window = browser.current_window_handle
 
-    # if q is pressed, quit
+    browser.switch_to.window(contacts_window)
+    # see https://www.testclass.cn/selenium_iframe.html
+    print("Waiting for the incoming call...")
+    start = time.time()
+    while(not hasElement(browser, receive_btn_xpath)):
+        end = time.time()
+        if((end - start) > 10):
+            print("The call is not received for 10 seconds.")
+            browser.quit()
+            return
+    browser.find_element(By.XPATH, receive_btn_xpath).click()
+    windows = browser.window_handles
+    call_window = windows[2]
+
+    browser.switch_to.window(call_window)
+    while(not hasElement(browser, caller_icon_xpath)):  # wait for the call box to be fully loaded
+        pass
+    print("The call is received.")
+
+    browser.switch_to.window(call_window)
+    start = time.time()
     while(True):
-        if(input() == 'q'):
+        end = time.time()
+        print("The call has been connected for " +
+              str(end - start) + " seconds.", end="\r")
+        # if q is pressed or 10 seconds have passed, quit the program
+        hasRecall = hasElement(browser, recall_btn_xpath)
+        if(((end - start) > 10) or hasRecall):
+            if(not hasRecall):
+                browser.find_element(By.XPATH, end_call_btn_xpath).click()
+            print("\nThe call is ended.")
+            browser.switch_to.window(rtc_window)
             browser.find_element(By.XPATH, download_btn_xpath1).click()
             browser.find_element(By.XPATH, download_btn_xpath2).click()
             sleep(3)  # wait for download to finish
+            print("The dump file is downloaded.")
             break
-    clear()
+
+    # clear()
     browser.quit()
 
 
